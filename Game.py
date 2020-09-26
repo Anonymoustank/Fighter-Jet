47ba84f16428f94fb19ae8a7ba4f27721d48904a
Anonymoustank <pkadekodi@gmail.com>
2020-09-26 10:40:22 -0600

    Added enemy, added health

--- Game.py
+++ Game.py
@@ -20,16 +20,20 @@ class Laser(pymunk.Segment):
         self.body.position = position
         self.body.angle = angle
         self.body.velocity = Vec2d(math.cos(self.body.angle), math.sin(self.body.angle)) * 800
+        self.damage = 25
 
 class Ship(pymunk.Poly):
-    def __init__(self):
-        self.vertices = ((-75, 25), (0, 0), (-75, -25))
+    def __init__(self, color = GREEN):
+        self.vertices = ((-50, 25), (0, 0), (-50, -25))
         super().__init__(None, (self.vertices))
         self.triangle_moment = pymunk.moment_for_poly(1, self.get_vertices())
         self.body = pymunk.Body(1, self.triangle_moment)
         self.position = WIDTH // 2, HEIGHT // 2
         self.laser_list = []
         self.cooldown = time.perf_counter()
+        self.sensor = True
+        self.color = color
+        self.health = 100
     def shoot(self):
         if abs(self.cooldown - time.perf_counter()) >= (1/4):
             self.cooldown = time.perf_counter()
@@ -44,6 +48,14 @@ class Ship(pymunk.Poly):
             space.add(self.laser_list[len(self.laser_list) - 1].body)
 
 player = Ship()
+enemy_list = []
+for i in range(1):
+    enemy_list.append(Ship(RED))
+    space.add(enemy_list[len(enemy_list) - 1])
+    space.add(enemy_list[len(enemy_list) - 1].body)
+    enemy_list[len(enemy_list) - 1].body.position = WIDTH // 2, HEIGHT // 2
+
+dead = False
 
 space.add(player, player.body)
 space.gravity = 0, 0
@@ -72,24 +84,49 @@ def on_draw():
     space.debug_draw(options)
 
 def refresh(time):
+    global dead
     if is_held_down == True:
         player.shoot()
-    for i in player.laser_list:
-        x_pos, y_pos = i.body.position
-        if x_pos > WIDTH or x_pos < 0 or y_pos > HEIGHT or y_pos < 0:
-            space.remove(i)
-            space.remove(i.body)
-            player.laser_list.remove(i)
-            
-    body_x, body_y = player.body.position
-    x, y = destination
-    player.body.angle = 0.0
-    player.body.angle = player.body.angle + math.atan2(body_y - y, body_x - x) - (math.pi)
-    distance = math.sqrt((x - body_x) ** 2 + (y - body_y) ** 2)
-    if distance >= 10:
-        player.body.velocity = Vec2d(math.cos(player.body.angle), math.sin(player.body.angle)) * 500
-    else:
-        player.body.velocity = 0, 0
+    
+    if dead == False:
+        for i in player.laser_list:
+            for enemy in enemy_list:
+                if len(i.shapes_collide(enemy).points) > 0:
+                    enemy.health -= i.damage
+                    if enemy.health <= 0:
+                        space.remove(enemy)
+                        space.remove(enemy.body)
+                        enemy_list.remove(enemy)
+                    space.remove(i)
+                    space.remove(i.body)
+                    player.laser_list.remove(i)
+            x_pos, y_pos = i.body.position
+            if x_pos > WIDTH or x_pos < 0 or y_pos > HEIGHT or y_pos < 0:
+                space.remove(i)
+                space.remove(i.body)
+                player.laser_list.remove(i)
+
+    if dead == False:
+        for enemy in enemy_list:
+            for enemy_laser in enemy.laser_list:
+                if len(enemy_laser.shapes_collide(player).points) > 0:
+                    player.health -= enemy_laser.damage
+                    if player.health <= 0:
+                        space.remove(player)
+                        space.remove(player.body)
+                        dead = True
+                    space.remove(enemy_laser)
+                    space.remove(enemy_laser.body)
+                    enemy.laser_list.remove(enemy_laser)
+        body_x, body_y = player.body.position
+        x, y = destination
+        player.body.angle = 0.0
+        player.body.angle = player.body.angle + math.atan2(body_y - y, body_x - x) - (math.pi)
+        distance = math.sqrt((x - body_x) ** 2 + (y - body_y) ** 2)
+        if distance >= 10:
+            player.body.velocity = Vec2d(math.cos(player.body.angle), math.sin(player.body.angle)) * 500
+        else:
+            player.body.velocity = 0, 0
     space.step(time)
 
 if __name__ == "__main__":
